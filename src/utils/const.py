3b5557ec9cb62e7a5b41c8feb9a89aa7c7493e64
Nimish <nimish@phase.dev>
2024-01-18 15:59:39 +0530

    fix: local ref pattern re

--- src/utils/const.py
+++ src/utils/const.py
@@ -1,6 +1,6 @@
 import os
 import re
-__version__ = "0.1"
+__version__ = "0.2"
 __ph_version__ = "v1"
 
 description = "Securely manage and sync environment variables with Phase."
@@ -35,4 +35,4 @@ pss_user_pattern = re.compile(r"^pss_user:v(\d+):([a-fA-F0-9]{64}):([a-fA-F0-9]{
 pss_service_pattern = re.compile(r"^pss_service:v(\d+):([a-fA-F0-9]{64}):([a-fA-F0-9]{64}):([a-fA-F0-9]{64}):([a-fA-F0-9]{64})$")
 
 cross_env_pattern = re.compile(r"\$\{(.+?)\.(.+?)\}")
-local_ref_pattern = re.compile(r"\$\{(.+?)\}")
+local_ref_pattern = re.compile(r"\$\{([^.]+?)\}")

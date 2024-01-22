e0c0b1ab131f6d51168bb48072caa88154eb4dc9
Nimish <nimish@phase.dev>
2024-01-22 13:51:05 +0530

    feat: simplified fetch

--- src/cmd/secrets/fetch.py
+++ src/cmd/secrets/fetch.py
@@ -1,15 +1,9 @@
 import sys
-import re
-import base64
-import json
-import yaml
 from src.utils.phase_io import Phase
-from src.utils.const import cross_env_pattern, local_ref_pattern
-from src.utils.misc import transform_name, process_secret
 
-def phase_secrets_fetch(phase_service_token=None, phase_service_host=None, env_name=None, phase_app=None, keys=None, tags=None, secret_format=None, name_transformer='upper_snake', secret_processor='b64'):
+def phase_secrets_fetch(phase_service_token=None, phase_service_host=None, env_name=None, phase_app=None, keys=None, tags=None):
     """
-    Fetch, decrypt, process and return secrets based on the provided environment, keys, tags, and format.
+    Fetch and return secrets based on the provided environment, keys, and tags.
     """
 
     phase = Phase(init=False, pss=phase_service_token, host=phase_service_host)
@@ -20,22 +14,5 @@ def phase_secrets_fetch(phase_service_token=None, phase_service_host=None, env_n
         print(f"Failed to fetch secrets: {e}")
         sys.exit(1)
 
-    secrets_dict = {}
-    for secret in secrets:
-        transformed_name = transform_name(secret["key"], name_transformer)
-        processed_secret = process_secret(secret["value"], secret_processor)
-        secrets_dict[transformed_name] = processed_secret
-
-    # Export based on selected format
-    if secret_format == 'json':
-        return json.dumps(secrets_dict)
-    elif secret_format == 'dotnet-json':
-        return json.dumps({key.replace('__', ':'): value for key, value in secrets_dict.items()})
-    elif secret_format == 'env':
-        return '\n'.join([f'{key}="{value}"' for key, value in secrets_dict.items()])
-    elif secret_format == 'yaml':
-        return yaml.dump(secrets_dict)
-    elif secret_format == 'docker':
-        return ' '.join([f'-e {key}="{value}"' for key, value in secrets_dict.items()])
-    else:
-        return secrets_dict
+    # Return secrets as a dictionary
+    return {secret["key"]: secret["value"] for secret in secrets}

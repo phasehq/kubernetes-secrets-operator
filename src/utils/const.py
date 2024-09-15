e84a4079b566620a1c1cf82e922d4b6b539bb3e8
Nimish <nimish@phase.dev>
2024-09-15 13:27:49 +0530

    chore: bump version

--- src/utils/const.py
+++ src/utils/const.py
@@ -1,6 +1,6 @@
 import os
 import re
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __ph_version__ = "v1"
 
 description = "Securely manage application secrets and environment variables with Phase."
